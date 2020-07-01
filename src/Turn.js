3bae12dfb754e1c052286ffa2e1c42d7139ceac3
Tyler Haglund <relyt4me@gmail.com>
2020-06-30 18:52:44 -0600

    Make Turn test check instatiation of Turn class

---
+++ src/Turn.js
@@ -0,0 +1,14 @@
+class Turn {
+  constructor(userGuess, card) {
+    this.userGuess = userGuess;
+    this.card = card;
+  }
+}
+module.exports = Turn;
+
+// [ ] Guess String argument
+// - [ ] Card object argument
+// - [ ] returnGuess
+// - [ ] returnCard
+// - [ ] evaluateGuess
+// - [ ] giveFeedback
--- test/Turn-test.js
+++ test/Turn-test.js
@@ -2,5 +2,21 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const Turn = require("../src/Turn");
+const Card = require("../src/Card");
 
-describe("Turn", function () {});
+describe("Turn", function () {
+  it("should be a function", function () {
+    expect(Turn).to.be.a("function");
+  });
+
+  it("should be an instance of Turn", function () {
+    const turn = new Turn();
+    expect(turn).to.be.an.instanceof(Turn);
+  });
+
+  it.skip("should store a userGuess", function () {
+    const turn = new Turn("answer");
+
+    expect(turn.userGuess).to.equal;
+  });
+});
